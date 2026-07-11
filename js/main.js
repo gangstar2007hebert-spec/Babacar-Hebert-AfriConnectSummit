b56f4523803d4f8092fc07c4ec36382808126722
Babacar <gangstar2007hebert@gmail.com>
2026-07-11 16:04:59 +0000

    Création de la page programme avec onglets dynamiques

--- js/main.js
+++ js/main.js
@@ -175,3 +175,27 @@ window.scrollTo({
 
 
 });
+const tabButtons = document.querySelectorAll(".tab-btn");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        tabButtons.forEach(btn =>
+            btn.classList.remove("active")
+        );
+
+        tabContents.forEach(content =>
+            content.classList.remove("active")
+        );
+
+        button.classList.add("active");
+
+        document
+            .getElementById(button.dataset.day)
+            .classList.add("active");
+
+    });
+
+});
